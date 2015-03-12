e6516ddef87841e520a8e9af0c074e47b6a1b279
Boy Baukema <boy@ibuildings.nl>
2015-03-12 16:28:36 +0100

    Use ?q=

--- src/themes/insecured7/page--front-not-logged-in.tpl.php
+++ src/themes/insecured7/page--front-not-logged-in.tpl.php
@@ -2,12 +2,12 @@
 <h2>Please log in.</h2>
 <?php
 $form = drupal_get_form('user_login');
-$form['#action']='/user';
+$form['#action']='?q=user';
 print render($form);
 ?>
 <h2>Or register a new account</h2>
 <?php
 $form = drupal_get_form('user_register_form');
-$form['#action']='/user/register';
+$form['#action']='?q=user/register';
 print render($form)
 ?>
